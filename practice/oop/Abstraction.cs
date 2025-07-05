d75d589dd3bf6f01d50ef8844872d8a87621825d
Kishan_Chauhan <kishanchauhan2006.25@gmail.com>
2025-07-05 12:25:38 +0530

    oop practice done

---
+++ practice/oop/Abstraction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Abstraction
+{
+    //abstract class
+    abstract class Shape
+    {
+        //abstract method
+        //no direct access
+        public abstract int area();
+    }
+
+    class Square : Shape
+    {
+        private int side;
+
+        public Square(int x = 0)
+        {
+            this.side = x;
+        }
+
+        public override int area()
+        {
+            Console.WriteLine("Area of Square: ");
+            return (side * side);
+        }
+    }
+
+    class MyClass
+    {
+        public static void Main(string[] args)
+        {
+            // creating reference of Shape class
+            // which refer to Square class instance
+            Shape s = new Square(4);
+
+            double result = s.area();
+
+            Console.Write("{0}", result);
+        }
+    }
+
+    
+}
---
+++ practice/oop/Dog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Dog
+{
+
+    string name;
+    string breed;
+    int age;
+    string color;
+
+    public Dog(string name, string breed, int age, string color)
+    {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+
+    public string getName()
+    {
+        return this.name;
+    }
+
+    public string getBreed()
+    {
+        return this.breed;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+
+    public string getColor()
+    {
+        return this.color;
+    }
+
+    public override string ToString()
+    {
+        return $"My name is {this.name} \n My breed is {this.breed} \n My age is {this.age} \n my color is {this.color}";
+    }
+
+
+    public static void Main(string[] args)
+    {
+        Dog tuffy = new Dog("tuffy", "papillon", 5, "white");
+        Console.WriteLine(tuffy.ToString());
+    }
+}
